5042b9699f3c6d2c57e69e5c42c39be4737ee1ab
Junfeng Ou <jun@Junfengs-MacBook-Pro.local>
2025-08-18 09:45:07 -0500

    Updated the map

--- pages/map/index.tsx
+++ pages/map/index.tsx
@@ -26,50 +26,61 @@ export default function MapPage() {
   return (
     <div
       style={{
-        height: '70vh',
-        width: '100%',
         display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        gap: 2
+        flexDirection: 'column',
+        width: '100%'
       }}
     >
+      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+        {'Franchise Finder'}
+      </h5>
       <div
         style={{
-          width: isMobile ? '100%' : '70%'
+          height: '70vh',
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          gap: 2
         }}
       >
-        <FranchiseFinder
-          investmentPositions={investmentPositions}
-          setInvestmentPositions={setInvestmentPositions}
-        />
-      </div>
-      {!isMobile && (
         <div
           style={{
-            display: 'flex',
-            flexDirection: 'column',
-            height: '70vh',
-            width: '30%',
-            gap: 2,
-            overflowX: 'scroll'
+            width: isMobile ? '100%' : '70%'
           }}
         >
-          {investmentPositions?.map((location) => {
-            return (
-              <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                  {location.franchiseName}
-                </h5>
-                <p className="font-normal text-gray-700 dark:text-gray-400">
-                  {location.capiSize}
-                </p>
-              </div>
-            );
-          })}
+          <FranchiseFinder
+            investmentPositions={investmentPositions}
+            setInvestmentPositions={setInvestmentPositions}
+          />
         </div>
-      )}
+        {!isMobile && (
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              height: '70vh',
+              width: '30%',
+              gap: 2,
+              overflowX: 'scroll'
+            }}
+          >
+            {investmentPositions?.map((location) => {
+              return (
+                <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                    {location.franchiseName}
+                  </h5>
+                  <p className="font-normal text-gray-700 dark:text-gray-400">
+                    {location.capiSize}
+                  </p>
+                </div>
+              );
+            })}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
